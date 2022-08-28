24592eafdd4566eb66a54b30c2da25ac336c4442
Jayraj Mulani <jayrajm.mulani@gmail.com>
2022-08-28 15:39:18 -0400

    Added 2 new test cases

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,22 @@
-def inc(x):
+def power(x, y):
+    return x**y
+
+
+def increment(x):
     return x + 1
 
 
-def test_answer():
-    assert inc(4) == 5
+def compare_length(x, y):
+    return len(x) == len(y)
+
+
+def test_increment():
+    assert increment(4) == 5
+
+
+def test_power():
+    assert power(2, 3) == 8
+
+
+def test_compare_length():
+    assert compare_length("1234", "abcd") == True
