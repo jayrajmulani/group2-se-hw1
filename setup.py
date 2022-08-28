6811939c041c7c894de43e689b14c92f6b93c30c
Harshil Sanghavi <harshilsan7@gmail.com>
2022-08-28 16:01:45 -0400

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='group2-se-hw1',
+    version='0.1',
+    description='Module for completing HW 1',
+    author='Harshil Sanghavi',
+    author_email='hsangha@ncsu.edu',
+    url='https://https://github.com/txt/se22/blob/main/docs/hw1.md',
+    packages=[],
+      long_description="""\
+        Module for setting up a Good repo and completing HW1
+      """,
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Development Status :: 1 - Planning",
+          "Intended Audience :: Developers",
+          "Natural Language :: English"
+      ],
+      keywords='test',
+      license='MIT',
+      install_requires=[
+        'virtualenv',
+      ],
+      )
